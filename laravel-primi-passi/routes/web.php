34282f6f3a8cdda4cd4feb0da1fcdc9ad39f4f5e
matteo-cota <matteocota@outlook.it>
2024-10-21 19:12:31 +0200

    added hello word dinamic

--- laravel-primi-passi/routes/web.php
+++ laravel-primi-passi/routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $message = 'Hello Word!';
+    return view('home', ['message' => $message]);
 });
